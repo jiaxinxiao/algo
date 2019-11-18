26f24b456f78d83b44f6f787b9179675533cc594
贾欣晓 <44742100+jiaxinxiao@users.noreply.github.com>
2019-11-18 11:55:42 +0800

    二叉线索存储结构

---
+++ datastructure/src/com/chapter3/tree/BiThrTree.java
@@ -0,0 +1,19 @@
+package com.chapter3.tree;
+/**
+ * 二叉线索存储结构
+ * 二叉线索树
+ * @author jiaxinxiao
+ * @date 2019年11月14日
+ */
+public class BiThrTree {
+	String data;//结点数据
+	
+	//左右结点指针
+	BiThrTree lchild;
+	BiThrTree rchild;
+	
+	//左右结点指针类型
+	PointerTag ltag;
+	PointerTag rtag;
+	
+}	
